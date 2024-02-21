2bfaeae76da1ad8cad880dce93f6c2078323981c
victor <devictoryghor@gmail.com>
2024-02-21 16:58:21 -0300

    feature: select the correct forge version and play the game

--- app_pojavlauncher/src/main/java/pixelmon/Pixelmon.kt
+++ app_pojavlauncher/src/main/java/pixelmon/Pixelmon.kt
@@ -4,11 +4,18 @@ import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
 import kotlinx.coroutines.runBlocking
+import net.kdt.pojavlaunch.extra.ExtraConstants
+import net.kdt.pojavlaunch.extra.ExtraCore
+import net.kdt.pojavlaunch.prefs.LauncherPreferences
 
 class Pixelmon(private val context: Context, popStack: () -> Boolean) {
     private val forgerInstaller = ForgerInstaller(context, popStack = popStack)
     fun start() {
 //        jreInstaller.installRequiredJRE()
         forgerInstaller.install()
+        LauncherPreferences.DEFAULT_PREF.edit()
+            .putString(LauncherPreferences.PREF_KEY_CURRENT_PROFILE, "1")
+            .apply()
+        ExtraCore.setValue(ExtraConstants.LAUNCH_GAME, true)
     }
 }
