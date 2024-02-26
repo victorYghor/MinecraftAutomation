9757cf706ceb18e817163898fcc74f8b23fdc090
victor <devictoryghor@gmail.com>
2024-02-26 16:34:14 -0300

    write in files

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.kt
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/MainMenuFragment.kt
@@ -1,81 +1,97 @@
-package net.kdt.pojavlaunch.fragments;
+package net.kdt.pojavlaunch.fragments
 
-import static net.kdt.pojavlaunch.Tools.shareLog;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.kdt.mcgui.mcVersionSpinner
+import net.kdt.pojavlaunch.CustomControlsActivity
+import net.kdt.pojavlaunch.R
+import net.kdt.pojavlaunch.Tools
+import net.kdt.pojavlaunch.extra.ExtraConstants
+import net.kdt.pojavlaunch.extra.ExtraCore
+import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper
+import pixelmon.Pixelmon
+import java.io.File
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
+class MainMenuFragment : Fragment(R.layout.fragment_launcher) {
+    private lateinit var mVersionSpinner: mcVersionSpinner
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mNewsButton = view.findViewById<Button>(R.id.news_button)
+        val mCustomControlButton = view.findViewById<Button>(R.id.custom_control_button)
+        val mInstallJarButton = view.findViewById<Button>(R.id.install_jar_button)
+        val mShareLogsButton = view.findViewById<Button>(R.id.share_logs_button)
+        val mTestButton = view.findViewById<Button>(R.id.test_button)
+        val mEditProfileButton = view.findViewById<ImageButton>(R.id.edit_profile_button)
+        val mPlayButton = view.findViewById<Button>(R.id.play_button)
+        mVersionSpinner = view.findViewById(R.id.mc_version_spinner)
+        mNewsButton.setOnClickListener { v: View? ->
+            Tools.openURL(
+                requireActivity(),
+                Tools.URL_HOME
+            )
+        }
+        mCustomControlButton.setOnClickListener { v: View? ->
+            startActivity(
+                Intent(
+                    requireContext(),
+                    CustomControlsActivity::class.java
+                )
+            )
+        }
+        mInstallJarButton.setOnClickListener { v: View? -> runInstallerWithConfirmation(false) }
+        mInstallJarButton.setOnLongClickListener { v: View? ->
+            runInstallerWithConfirmation(true)
+            true
+        }
+        mEditProfileButton.setOnClickListener { v: View? ->
+            mVersionSpinner.openProfileEditor(
+                requireActivity()
+            )
+        }
+        mPlayButton.setOnClickListener { v: View? ->
+            ExtraCore.setValue(
+                ExtraConstants.LAUNCH_GAME,
+                true
+            )
+        }
+        mShareLogsButton.setOnClickListener { v: View? -> Tools.shareLog(requireContext()) }
+        val pixelmon = Pixelmon(requireContext()) { parentFragmentManager.popBackStackImmediate() }
+        mTestButton.setOnClickListener { v: View? -> pixelmon.start() }
+        mNewsButton.setOnLongClickListener { v: View? ->
+            Tools.swapFragment(
+                requireActivity(),
+                SearchModFragment::class.java,
+                SearchModFragment.TAG,
+                true,
+                null
+            )
+            true
+        }
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import com.kdt.mcgui.mcVersionSpinner;
-
-import net.kdt.pojavlaunch.CustomControlsActivity;
-import net.kdt.pojavlaunch.R;
-import net.kdt.pojavlaunch.Tools;
-import net.kdt.pojavlaunch.extra.ExtraConstants;
-import net.kdt.pojavlaunch.extra.ExtraCore;
-import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
-
-import pixelmon.Pixelmon;
-
-public class MainMenuFragment extends Fragment {
-    public static final String TAG = "MainMenuFragment";
-
-    private mcVersionSpinner mVersionSpinner;
-
-    public MainMenuFragment() {
-        super(R.layout.fragment_launcher);
+        val inputStream = context?.assets?.open("components/test.txt")
+        val text = inputStream?.bufferedReader().use { it?.readText() }
+        inputStream?.close()
+        val outFile = File(context?.getExternalFilesDir(null), "/caciocavallo/testOut.txt")
+        outFile.writeText(text ?: "")
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Button mNewsButton = view.findViewById(R.id.news_button);
-        Button mCustomControlButton = view.findViewById(R.id.custom_control_button);
-        Button mInstallJarButton = view.findViewById(R.id.install_jar_button);
-        Button mShareLogsButton = view.findViewById(R.id.share_logs_button);
-        Button mTestButton = view.findViewById(R.id.test_button);
-
-        ImageButton mEditProfileButton = view.findViewById(R.id.edit_profile_button);
-        Button mPlayButton = view.findViewById(R.id.play_button);
-        mVersionSpinner = view.findViewById(R.id.mc_version_spinner);
-
-        mNewsButton.setOnClickListener(v -> Tools.openURL(requireActivity(), Tools.URL_HOME));
-        mCustomControlButton.setOnClickListener(v -> startActivity(new Intent(requireContext(), CustomControlsActivity.class)));
-        mInstallJarButton.setOnClickListener(v -> runInstallerWithConfirmation(false));
-        mInstallJarButton.setOnLongClickListener(v -> {
-            runInstallerWithConfirmation(true);
-            return true;
-        });
-        mEditProfileButton.setOnClickListener(v -> mVersionSpinner.openProfileEditor(requireActivity()));
-        mPlayButton.setOnClickListener(v -> ExtraCore.setValue(ExtraConstants.LAUNCH_GAME, true));
-        mShareLogsButton.setOnClickListener((v) -> shareLog(requireContext()));
-
-        Pixelmon pixelmon = new Pixelmon(requireContext(), () -> getParentFragmentManager().popBackStackImmediate());
-            mTestButton.setOnClickListener((v) ->
-                    pixelmon.start());
-
-        mNewsButton.setOnLongClickListener((v) -> {
-            Tools.swapFragment(requireActivity(), SearchModFragment.class, SearchModFragment.TAG, true, null);
-            return true;
-        });
+    override fun onResume() {
+        super.onResume()
+        mVersionSpinner.reloadProfiles()
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mVersionSpinner.reloadProfiles();
+    private fun runInstallerWithConfirmation(isCustomArgs: Boolean) {
+        if (ProgressKeeper.getTaskCount() == 0) Tools.installMod(
+            requireActivity(),
+            isCustomArgs
+        ) else Toast.makeText(requireContext(), R.string.tasks_ongoing, Toast.LENGTH_LONG).show()
     }
 
-    private void runInstallerWithConfirmation(boolean isCustomArgs) {
-        if (ProgressKeeper.getTaskCount() == 0)
-            Tools.installMod(requireActivity(), isCustomArgs);
-        else
-            Toast.makeText(requireContext(), R.string.tasks_ongoing, Toast.LENGTH_LONG).show();
+    companion object {
+        const val TAG = "MainMenuFragment"
     }
 }
