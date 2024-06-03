3d0cbac526908c0950a873fcd38958b04814ba53
vitorYghor <devictoryghor@gmail.com>
2024-06-03 08:39:30 -0300

    feature: update the progress with a string

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/progresskeeper/ProgressListener.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/progresskeeper/ProgressListener.java
@@ -5,6 +5,6 @@ package net.kdt.pojavlaunch.progresskeeper;
  */
 public interface ProgressListener {
     void onProgressStarted();
-    void onProgressUpdated(int progress, int resid, Object... va);
+    void onProgressUpdated(int progress, String message, Object... va);
     void onProgressEnded();
 }
